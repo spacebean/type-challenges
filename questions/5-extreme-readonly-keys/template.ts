bf9803a3eda5e189933c83cfdca6e0c1e3657d53
spacebean <spacebean@outlook.com>
2021-08-27 15:38:45 +0300

    Extreme playing.

--- questions/5-extreme-readonly-keys/template.ts
+++ questions/5-extreme-readonly-keys/template.ts
@@ -1,1 +1,5 @@
-type GetReadonlyKeys<T> = any
+import { Equal } from '@type-challenges/utils';
+
+export type GetReadonlyKeys<T> = keyof {
+  [K in keyof T as Equal<{ [_ in K]: T[K] }, { readonly [_ in K]: T[K] }> extends true ? K : never]: T[K];
+};
