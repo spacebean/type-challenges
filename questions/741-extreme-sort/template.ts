f4a06820c6f4451cd6d7fb80cf5ad0b26e0e4a29
spacebean <spacebean@outlook.com>
2021-08-31 10:53:51 +0300

    Extreme playing.

--- questions/741-extreme-sort/template.ts
+++ questions/741-extreme-sort/template.ts
@@ -1,1 +1,24 @@
-type Sort = any
+import { Comparator, Comparison } from '../274-extreme-integers-comparator/template';
+
+type Greater<X extends number, Y extends number> = Comparator<X, Y> extends Comparison.Greater ? true : false;
+
+type IsNumber<T> = T extends number ? T : never;
+
+type Exchange<T extends any[], P extends boolean, Temp extends any[] = []> = T extends [
+  ...infer Rest,
+  infer PreLast,
+  infer Last
+]
+  ? Greater<IsNumber<Last>, IsNumber<PreLast>> extends P
+    ? Exchange<[...Rest, Last], P, [PreLast, ...Temp]>
+    : [...T, ...Temp]
+  : [...T, ...Temp];
+
+type InsertionSort<T extends any[], P extends boolean, Result extends any[] = []> = T extends [
+  infer First,
+  ...infer Rest
+]
+  ? InsertionSort<Rest, P, Exchange<[...Result, First], P>>
+  : Result;
+
+export type Sort<T extends any[], P extends boolean = false> = InsertionSort<T, P>;
