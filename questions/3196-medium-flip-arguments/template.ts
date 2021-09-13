2938ffb75a2a960fc52e76a41d6e7b1457ce9af2
spacebean <spacebean@outlook.com>
2021-09-13 12:53:05 +0300

    Medium playing.

--- questions/3196-medium-flip-arguments/template.ts
+++ questions/3196-medium-flip-arguments/template.ts
@@ -1,1 +1,3 @@
-type FlipArguments<T> = any
+import { Reverse } from '../3192-medium-reverse/template';
+
+export type FlipArguments<T> = T extends (...args: infer I) => infer O ? (...args: Reverse<I>) => O : never;
