64e187814a54a0baa082fac27a6b69d3afcfab2d
spacebean <spacebean@outlook.com>
2021-09-16 11:00:45 +0300

    Medium playing.

--- questions/3243-medium-flattendepth/template.ts
+++ questions/3243-medium-flattendepth/template.ts
@@ -1,1 +1,9 @@
-type FlattenDepth = any
+type FlattenDepthImpl<T, N extends number, Temp extends any[] = []> = T extends [any, ...infer Rest]
+  ? Temp['length'] extends N
+    ? T
+    : T[0] extends any[]
+    ? [...FlattenDepthImpl<T[0], N, [[], ...Temp]>, ...FlattenDepthImpl<Rest, N, Temp>]
+    : [T[0], ...FlattenDepthImpl<Rest, N, Temp>]
+  : [];
+
+export type FlattenDepth<T, N extends number = 1> = FlattenDepthImpl<T, N>;
