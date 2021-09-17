2247c33145059f8c129bb963dd79aed7ae71d8e7
spacebean <spacebean@outlook.com>
2021-09-17 09:09:21 +0300

    Refactor code.

--- questions/3243-medium-flattendepth/template.ts
+++ questions/3243-medium-flattendepth/template.ts
@@ -1,9 +1,7 @@
-type FlattenDepthImpl<T, N extends number, Temp extends any[] = []> = T extends [any, ...infer Rest]
+export type FlattenDepth<T, N extends number = 1, Temp extends any[] = []> = T extends [any, ...infer Rest]
   ? Temp['length'] extends N
     ? T
     : T[0] extends any[]
-    ? [...FlattenDepthImpl<T[0], N, [[], ...Temp]>, ...FlattenDepthImpl<Rest, N, Temp>]
-    : [T[0], ...FlattenDepthImpl<Rest, N, Temp>]
+    ? [...FlattenDepth<T[0], N, [[], ...Temp]>, ...FlattenDepth<Rest, N, Temp>]
+    : [T[0], ...FlattenDepth<Rest, N, Temp>]
   : [];
-
-export type FlattenDepth<T, N extends number = 1> = FlattenDepthImpl<T, N>;
