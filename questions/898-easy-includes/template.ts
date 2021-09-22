5e748c80a2823c4f380c675fac7c194ec608e625
spacebean <spacebean@outlook.com>
2021-09-22 14:21:02 +0300

    Easy playing.

--- questions/898-easy-includes/template.ts
+++ questions/898-easy-includes/template.ts
@@ -1,7 +1,7 @@
+import { Equal } from '@type-challenges/utils';
+
 export type Includes<T extends readonly any[], U> = T extends [infer First, ...infer Rest]
-  ? [First] extends [U]
-    ? [U] extends [First]
-      ? true
-      : Includes<Rest, U>
+  ? Equal<First, U> extends true
+    ? true
     : Includes<Rest, U>
   : false;
