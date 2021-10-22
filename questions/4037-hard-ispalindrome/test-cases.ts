bd588f3fa9d3110e76eef27ddf22fc2b2b72a992
spacebean <spacebean@outlook.com>
2021-10-22 13:54:28 +0300

    Hard playing.

--- questions/4037-hard-ispalindrome/test-cases.ts
+++ questions/4037-hard-ispalindrome/test-cases.ts
@@ -1,9 +1,11 @@
-import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+import { Equal, Expect } from '@type-challenges/utils';
+import { IsPalindrome } from './template';
 
+// noinspection JSUnusedLocalSymbols
 type cases = [
   Expect<Equal<IsPalindrome<'abc'>, false>>,
   Expect<Equal<IsPalindrome<'b'>, true>>,
   Expect<Equal<IsPalindrome<'abca'>, false>>,
   Expect<Equal<IsPalindrome<121>, true>>,
-  Expect<Equal<IsPalindrome<19260817>, false>>,
-]
+  Expect<Equal<IsPalindrome<19260817>, false>>
+];
