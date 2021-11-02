2ace4dfe27c89922a1c96c1dae6e15e243d0616a
spacebean <spacebean@outlook.com>
2021-11-02 23:06:41 +0300

    Medium playing.

--- questions/4182-medium-fibonacci-sequence/template.ts
+++ questions/4182-medium-fibonacci-sequence/template.ts
@@ -1,1 +1,5 @@
-type Fibonacci<T extends number> = any
+import { MinusOne } from '../2257-medium-minusone/template';
+
+export type Fibonacci<T extends number, Prev extends any[] = [], Next extends any[] = [any]> = T extends 0
+  ? Prev['length']
+  : Fibonacci<MinusOne<T>, Next, [...Prev, ...Next]>;
