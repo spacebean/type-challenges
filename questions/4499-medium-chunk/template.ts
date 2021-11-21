5b49962659a1d85feae9ca66ce9211a0a9d5a075
spacebean <spacebean@outlook.com>
2021-11-21 12:17:47 +0300

    Medium playing

--- questions/4499-medium-chunk/template.ts
+++ questions/4499-medium-chunk/template.ts
@@ -1,1 +1,18 @@
-type Chunk = any
+type SplitAt<T extends any[], N extends number, R extends [any[], any[]] = [[], []]> = R[1]['length'] extends N
+  ? R
+  : T extends [infer First, ...infer Rest]
+  ? SplitAt<Rest, N, [Rest, [...R[1], First]]>
+  : R;
+
+type Chunk<
+  T extends any[],
+  N extends number,
+  R extends any[] = [],
+  Temp extends [any[], any[]] = SplitAt<T, N>
+> = Temp extends [[], []]
+  ? R
+  : Temp extends [[...any], [...any]]
+  ? Chunk<Temp[0], N, [...R, Temp[1]]>
+  : Temp extends [[], [...any]]
+  ? [...R, Temp[1]]
+  : R;
