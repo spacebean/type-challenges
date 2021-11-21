052c61a114b818f5421c91093bd99ecc7636932d
spacebean <spacebean@outlook.com>
2021-11-21 12:43:30 +0300

    Medium playing

--- questions/4518-medium-fill/template.ts
+++ questions/4518-medium-fill/template.ts
@@ -1,6 +1,18 @@
+type Or<X extends boolean, Y extends boolean> = X extends true ? true : Y extends true ? true : false;
+
 type Fill<
   T extends unknown[],
   N,
   Start extends number = 0,
   End extends number = T['length'],
-> = any
+  InRange extends boolean = false,
+  Index extends any[] = []
+> = Start extends End
+  ? T
+  : Index['length'] extends End
+  ? T
+  : T extends [infer First, ...infer Rest]
+  ? Or<Index['length'] extends Start ? true : false, InRange> extends true
+    ? [N, ...Fill<Rest, N, Start, End, true, [...Index, any]>]
+    : [First, ...Fill<Rest, N, Start, End, false, [...Index, any]>]
+  : T;
--- questions/4518-medium-fill/test-cases.ts
+++ questions/4518-medium-fill/test-cases.ts
@@ -1,5 +1,6 @@
-import { Equal, Expect } from '@type-challenges/utils'
+import { Equal, Expect } from '@type-challenges/utils';
 
+// noinspection JSUnusedLocalSymbols
 type cases = [
   Expect<Equal<Fill<[], 0>, []>>,
   Expect<Equal<Fill<[], 0, 0, 3>, []>>,
@@ -10,5 +11,5 @@ type cases = [
   Expect<Equal<Fill<[1, 2, 3], true, 0, 1>, [true, 2, 3]>>,
   Expect<Equal<Fill<[1, 2, 3], true, 1, 3>, [1, true, true]>>,
   Expect<Equal<Fill<[1, 2, 3], true, 10, 0>, [1, 2, 3]>>,
-  Expect<Equal<Fill<[1, 2, 3], true, 0, 10>, [true, true, true]>>,
-]
+  Expect<Equal<Fill<[1, 2, 3], true, 0, 10>, [true, true, true]>>
+];
