f1890e4194890dc82d0be53aad240b8fe17164a0
spacebean <spacebean@outlook.com>
2022-02-05 12:55:29 +0300

    Reformat code

--- questions/12-medium-chainable-options/test-cases.ts
+++ questions/12-medium-chainable-options/test-cases.ts
@@ -3,31 +3,21 @@ import { Chainable } from './template';
 
 declare const a: Chainable;
 
-const result1 = a
-  .option('foo', 123)
-  .option('bar', { value: 'Hello World' })
-  .option('name', 'type-challenges')
-  .get();
-
-const result2 = a
-  .option('name', 'another name')
-  // @ts-expect-error
-  .option('name', 'last name')
-  .get();
-
-type cases = [
-  Expect<Alike<typeof result1, Expected1>>,
-  Expect<Alike<typeof result2, Expected2>>,
-];
-
 type Expected1 = {
-  foo: number
+  foo: number;
   bar: {
-    value: string
-  }
-  name: string
+    value: string;
+  };
+  name: string;
 };
 
 type Expected2 = {
-  name: string
+  name: string;
 };
+
+const result1 = a.option('foo', 123).option('bar', { value: 'Hello World' }).option('name', 'type-challenges').get();
+
+const result2 = a.option('name', 'another name').option('name', 'last name').get();
+
+// noinspection JSUnusedLocalSymbols
+type cases = [Expect<Alike<typeof result1, Expected1>>, Expect<Alike<typeof result2, Expected2>>];
