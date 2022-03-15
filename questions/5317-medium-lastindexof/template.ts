2001a6ffd58a6a25779b0c3357d3bd13dae50a96
spacebean <spacebean@outlook.com>
2022-03-15 18:54:50 +0300

    Medium playing

--- questions/5317-medium-lastindexof/template.ts
+++ questions/5317-medium-lastindexof/template.ts
@@ -1,1 +1,8 @@
-type LastIndexOf<T, U> = any;
+export type LastIndexOf<T extends unknown[], U extends number> = T extends [
+  ...infer Head,
+  infer Last
+]
+  ? Last extends U
+    ? Head['length']
+    : LastIndexOf<Head, U>
+  : -1;
--- questions/5317-medium-lastindexof/test-cases.ts
+++ questions/5317-medium-lastindexof/test-cases.ts
@@ -1,5 +1,7 @@
 import { Equal, Expect } from '@type-challenges/utils';
+import { LastIndexOf } from './template';
 
+// noinspection JSUnusedLocalSymbols
 type cases = [
   Expect<Equal<LastIndexOf<[1, 2, 3, 2, 1], 2>, 3>>,
   Expect<Equal<LastIndexOf<[2, 6, 3, 8, 4, 1, 7, 3, 9], 3>, 7>>,
