9a5e628840fbd3eb749c8220d23175a44845cf18
spacebean <spacebean@outlook.com>
2022-03-15 09:05:42 +0300

    Medium playing

--- questions/5117-medium-without/template.ts
+++ questions/5117-medium-without/template.ts
@@ -1,1 +1,13 @@
-type Without<T, U> = any;
+type Contains<T, U> = T extends [infer Head, ...infer Tail]
+  ? Head extends U
+    ? true
+    : Contains<Tail, U>
+  : T extends U
+  ? true
+  : false;
+
+export type Without<T, U> = T extends [infer Head, ...infer Tail]
+  ? Contains<U, Head> extends true
+    ? Without<Tail, U>
+    : [Head, ...Without<Tail, U>]
+  : [];
--- questions/5117-medium-without/test-cases.ts
+++ questions/5117-medium-without/test-cases.ts
@@ -1,5 +1,7 @@
 import { Equal, Expect } from '@type-challenges/utils';
+import { Without } from './template';
 
+// noinspection JSUnusedLocalSymbols
 type cases = [
   Expect<Equal<Without<[1, 2], 1>, [2]>>,
   Expect<Equal<Without<[1, 2, 4, 1, 5], [1, 2]>, [4, 5]>>,
