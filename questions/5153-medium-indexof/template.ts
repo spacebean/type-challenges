9fa4b3c242e95175c2c596429251cdc3540a275b
spacebean <spacebean@outlook.com>
2022-03-15 17:02:27 +0300

    Medium playing

--- questions/5153-medium-indexof/template.ts
+++ questions/5153-medium-indexof/template.ts
@@ -1,1 +1,5 @@
-type IndexOf<T, U> = any;
+export type IndexOf<T, U, Index extends unknown[] = []> = T extends [infer Head, ...infer Tail]
+  ? Head extends U
+    ? Index['length']
+    : IndexOf<Tail, U, [...Index, unknown]>
+  : -1;
