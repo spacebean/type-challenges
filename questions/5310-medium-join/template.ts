1c6fe0410f265c82fff70bc9807ac8cc9bf99bdb
spacebean <spacebean@outlook.com>
2022-03-15 17:45:06 +0300

    Medium playing

--- questions/5310-medium-join/template.ts
+++ questions/5310-medium-join/template.ts
@@ -1,1 +1,5 @@
-type Join<T, U> = any;
+export type Join<T extends unknown[], U extends string | number> = T extends [unknown]
+  ? T[0]
+  : T extends [string, ...infer Tail]
+  ? `${T[0]}${U}${Join<Tail, U>}`
+  : '';
--- questions/5310-medium-join/test-cases.ts
+++ questions/5310-medium-join/test-cases.ts
@@ -1,5 +1,7 @@
 import { Equal, Expect } from '@type-challenges/utils';
+import { Join } from './template';
 
+// noinspection JSUnusedLocalSymbols
 type cases = [
   Expect<Equal<Join<['a', 'p', 'p', 'l', 'e'], '-'>, 'a-p-p-l-e'>>,
   Expect<Equal<Join<['Hello', 'World'], ' '>, 'Hello World'>>,
