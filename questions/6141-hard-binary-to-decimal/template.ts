2e6a3ccec575150b4f6eb68804e09a3ef332d981
spacebean <spacebean@outlook.com>
2022-03-16 17:54:01 +0300

    Hard playing

--- questions/6141-hard-binary-to-decimal/template.ts
+++ questions/6141-hard-binary-to-decimal/template.ts
@@ -1,1 +1,10 @@
-type BinaryToDecimal<S extends string> = any;
+type Doubling<T extends unknown[]> = [...T, ...T];
+
+export type BinaryToDecimal<
+  S extends string,
+  Result extends unknown[] = []
+> = S extends `${infer First}${infer Rest}`
+  ? First extends '0'
+    ? BinaryToDecimal<Rest, Doubling<Result>>
+    : BinaryToDecimal<Rest, [...Doubling<Result>, unknown]>
+  : Result['length'];
--- questions/6141-hard-binary-to-decimal/test-cases.ts
+++ questions/6141-hard-binary-to-decimal/test-cases.ts
@@ -1,7 +1,11 @@
 import { Equal, Expect } from '@type-challenges/utils';
+import { BinaryToDecimal } from './template';
 
+// noinspection JSUnusedLocalSymbols
 type cases = [
   Expect<Equal<BinaryToDecimal<'10'>, 2>>,
+  Expect<Equal<BinaryToDecimal<'100'>, 4>>,
+  Expect<Equal<BinaryToDecimal<'01'>, 1>>,
   Expect<Equal<BinaryToDecimal<'0011'>, 3>>,
   Expect<Equal<BinaryToDecimal<'00000000'>, 0>>,
   Expect<Equal<BinaryToDecimal<'11111111'>, 255>>,
