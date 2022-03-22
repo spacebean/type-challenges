a0224677a299cb804eab702ff1db9491574c318c
spacebean <spacebean@outlook.com>
2022-03-22 13:40:53 +0300

    Hard playing

--- questions/7258-hard-object-key-paths/template.ts
+++ questions/7258-hard-object-key-paths/template.ts
@@ -1,1 +1,11 @@
-type ObjectKeyPaths<T extends object> = any;
+export type ObjectKeyPaths<T extends object> = keyof {
+  [K in keyof T as T[K] extends object
+    ?
+        | K
+        | `${K extends string ? K : never}.${ObjectKeyPaths<T[K]> extends infer I
+            ? I extends string | number | symbol
+              ? I
+              : never
+            : never}`
+    : K]: never;
+};
