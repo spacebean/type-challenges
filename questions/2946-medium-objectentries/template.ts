e3b8cb0d1d4af82919b78e1ca55f1edd939ebd38
spacebean <spacebean@outlook.com>
2022-04-03 10:16:26 +0300

    Medium playing

--- questions/2946-medium-objectentries/template.ts
+++ questions/2946-medium-objectentries/template.ts
@@ -1,3 +1,5 @@
+import { IsNever } from '../1042-medium-isnever/template';
+
 export type ObjectEntries<T, R = Required<T>> = {
-  [K in keyof R]: [K, R[K]];
+  [K in keyof R]: [K, IsNever<R[K]> extends true ? undefined : R[K]];
 }[keyof R];
