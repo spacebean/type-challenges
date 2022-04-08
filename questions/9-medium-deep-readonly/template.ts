97e0fa35c32e3eb8b2e8b9a6668953e858c8fdee
spacebean <spacebean@outlook.com>
2022-04-08 10:58:06 +0300

    Medium playing

--- questions/9-medium-deep-readonly/template.ts
+++ questions/9-medium-deep-readonly/template.ts
@@ -1,6 +1,8 @@
 export type DeepReadonly<T> = {
   readonly [K in keyof T]: T[K] extends Function
     ? T[K]
+    : T[K] extends [any]
+    ? T[K]
     : T[K] extends object
     ? DeepReadonly<T[K]>
     : T[K];
